dc7dbf389347f56c2cde864ef1a20d960ab13176
Faysal Ahmed <kawsar.ahmed042@gmail.com>
2017-11-08 11:48:40 +0600

    example added

---
+++ example/App.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+//import * as test from '@faysal/gifted-xmpp'
+import {StackNavigator, TabNavigator} from 'react-navigation'
+
+import {Provider} from 'react-redux'
+import Login from './js/components/login'
+import JobListView from './js/components/jobListView'
+import Messenger from './js/components/messenger'
+import store from './js/store'
+import xmpp from 'sendjobs-xmpp/chatHandler'
+
+const config = {
+  domain : 'sendjob',
+  host: 'localhost',
+  port: 5222,
+  schema: 'mobile',
+  authType: 0 // 0 -> plain auth, 1-> scram, 2-> md5digest
+}
+const Main = StackNavigator({
+  Login: {
+    screen: Login
+  },
+  JobList: {
+    screen: JobListView
+  },
+  Messenger: {
+    screen: Messenger
+  },
+})
+
+export default class App extends Component<{}> {
+  xmpp = null
+  componentDidMount() {
+    this.xmpp = new xmpp(config)
+  }
+
+  componentWillUnmount() {
+    //XMPP.removeListeners();
+    //XMPP.removeListener('message');
+  }
+
+  render() {
+
+    return (
+      <Provider store={store}>
+        <View style={styles.container} >
+          <Main screenProps={{xmpp:new xmpp(config)}}/>
+        </View>
+      </Provider>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ example/js/components/messenger.js
@@ -0,0 +1,160 @@
+import React, {Component} from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native'
+import {connect} from 'react-redux'
+import uuidv4 from 'uuid/v4'
+
+import {SystemMessage} from 'react-native-gifted-chat'
+import XMPPMessenger from 'sendjobs-xmpp'
+
+class Messenger extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: [],
+    };
+
+    this.renderSystemMessage = this.renderSystemMessage.bind(this);
+    this.onSend = this.onSend.bind(this)
+  }
+
+  onReceiveMessage(text) {
+    if (!text.body)
+      return
+
+    this.props.receiveMessage({
+      _id: uuidv4(),
+      text: text.body,
+      createdAt: new Date(),
+      user: {
+        _id: text.from.split('@')[0]
+      }
+    })
+
+  }
+
+  onSend(msg) {
+    let {params} = this.props.navigation.state
+    msg.recipient = params.user.publicKey
+    this.props.sendMessage(msg)
+  }
+
+  renderSystemMessage(props) {
+    return (
+      <SystemMessage
+        {...props}
+        containerStyle={{
+          marginBottom: 15,
+        }}
+        textStyle={{
+          fontSize: 25,
+          // textDecorationLine: 'underline'
+        }}
+      />
+    );
+  }
+
+
+
+  onPressPhoneNumber() {
+    console.log('click ulr')
+  }
+
+  render() {
+    return (
+      <XMPPMessenger
+        xmpp={this.props.screenProps.xmpp}
+        chat={this.props.chat}
+        chattingWith={this.props.navigation.state.params.user.publicKey}
+        onSend={this.onSend}
+        onReceiveMessage={this.onReceiveMessage.bind(this)}
+        user={{
+          _id: this.props.chat.jid
+        }}
+        renderSystemMessage={this.renderSystemMessage}
+        parsePatterns={(linkStyle) => [
+          {type: 'url', style: {...linkStyle}, onPress: this.onPressPhoneNumber},
+        ]}
+        systemMessageParsePatterns={
+          (currentMessage) => [
+            {
+              pattern: /[a-zA-Z0-9]+ applied for the job/,
+              style: {backgroundColor: 'black', color: 'yellow'},
+              onPress: (x) => console.log('QQQ ', x, this.props,currentMessage) // url opener function here
+            },
+          ]
+        }
+      />
+    )
+  }
+}
+
+
+const mapStateToProps = (store, ownProps) => {
+  return {
+    chat: store.chat
+  }
+}
+
+const mapDisPatchToProps = (dispatch, ownProps) => {
+  return {
+    sendMessage: (msg) => dispatch({type: 'MESSAGE_SENT', payload: msg}),
+    receiveMessage: (msg) => dispatch({type: 'MESSAGE_RECEIVED', payload: msg})
+  }
+}
+
+export default connect(mapStateToProps, mapDisPatchToProps)(Messenger)
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  url: {
+    color: 'red',
+    textDecorationLine: 'underline',
+  },
+
+  email: {
+    textDecorationLine: 'underline',
+  },
+
+  text: {
+    color: 'black',
+    fontSize: 15,
+  },
+
+  phone: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+
+  name: {
+    color: 'red',
+  },
+
+  username: {
+    color: 'green',
+    fontWeight: 'bold'
+  },
+
+  magicNumber: {
+    fontSize: 42,
+    color: 'pink',
+  },
+
+  hashTag: {
+    fontStyle: 'italic',
+  },
+
+});
+
